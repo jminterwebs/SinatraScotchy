959643abad9600f37b4363da8d7ac9e63528c853
Unknown <jmverdone@gmail.com>
2016-12-20 21:26:14 -0500

    flavor and distiller schema

--- db/migrate/20161221021530_create_distiller.rb
+++ db/migrate/20161221021530_create_distiller.rb
@@ -1,4 +1,8 @@
 class CreateDistiller < ActiveRecord::Migration
   def change
+    create_table :distiller do |t|
+      t.string :name
+      t.integer :scotch_id
+    end
   end
 end
--- db/migrate/20161221021539_create_flavor.rb
+++ db/migrate/20161221021539_create_flavor.rb
@@ -1,4 +1,7 @@
 class CreateFlavor < ActiveRecord::Migration
   def change
+    create_table :flavors do |t|
+      t.string :name
+    end
   end
 end
