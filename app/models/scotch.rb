65b5a042a4e42462597aea3afa5182fb32036fbd
Unknown <jmverdone@gmail.com>
2016-12-20 22:01:36 -0500

    set up many to many relationship users and scotch

--- app/models/scotch.rb
+++ app/models/scotch.rb
@@ -1,3 +1,4 @@
 class Scotch < ActiveRecord::Base
-
+  has_many :user_scotches
+  has_many :users, through: :user_scotches
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  has_many :user_scotches
+  has_many :scotches, through: :user_scotches
+  
 end
