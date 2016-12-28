01ae3a2bf8766b68ea81deac8cb2a1d18f484ab9
Unknown <jmverdone@gmail.com>
2016-12-27 21:28:43 -0500

    set up signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    erb :'user/new'
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:password] == "" || params[:email] == ""
+      redirect to "/signup"
+    else
+      User.create(username: params[:username], email: params[:email], password: params[:password])
+      @user = User.find_by(username: params[:username])
+      session[:user_id] = @user.id
+      redirect to '/'
+    end
+
+
+
+  end
+
 end
