8b07735315b4b4ad563d1dbd0153dce8318ceb06
Unknown <jmverdone@gmail.com>
2017-01-02 17:05:25 -0500

    set up adding scotch on creation

--- app/controllers/scotch_controller.rb
+++ app/controllers/scotch_controller.rb
@@ -10,10 +10,12 @@ class ScotchController < ApplicationController
   end
 
   post '/new' do
+    @user = User.find_by_id(current_user.id)
     if params[:name] == "" || params[:age] == "" || params[:abv] == ""
       redirect to "/signup"
     else
-      Scotch.create(name: params[:name], age: params[:age], abv: params[:abv], region: params[:region])
+      @scotch  = Scotch.create(name: params[:name], age: params[:age], abv: params[:abv], region: params[:region])
+      @scotch.users << @user
       redirect to '/'
     end
   end
