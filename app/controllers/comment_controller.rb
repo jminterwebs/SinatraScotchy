490398bf012a31545c28b1d081b22a8c16c2d419
Unknown <jmverdone@gmail.com>
2017-01-11 21:24:02 -0500

    refacting whitespace

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -8,9 +8,6 @@ class CommentController < ApplicationController
   post '/scotch/:slug/comments' do
     @scotch = Scotch.find_by_slug(params[:slug])
     Comment.create(content: params[:comment], scotch_id: @scotch.id, user_id: current_user.id)
-
-
-
   end
 
 end
