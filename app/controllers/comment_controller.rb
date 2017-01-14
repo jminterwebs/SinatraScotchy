120fe2eecf8e0bea3eee404e1046b87cd6dce06e
Unknown <jmverdone@gmail.com>
2017-01-14 18:24:14 -0500

    adding comment functionality

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,13 +1,20 @@
+require 'pry'
+
 class CommentController < ApplicationController
 
   get '/scotch/:slug/comments' do
     @scotch = Scotch.find_by_slug(params[:slug])
-    erb :'/scotch/edit'
+    binding.pry
+    erb :'/comments/new'
   end
 
   post '/scotch/:slug/comments' do
     @scotch = Scotch.find_by_slug(params[:slug])
+
+
     Comment.create(content: params[:comment], scotch_id: @scotch.id, user_id: current_user.id)
+
+    redirect to  "/scotch/#{@scotch.slug}"
   end
 
 end
