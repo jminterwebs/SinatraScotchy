ffc2fb4c705a8e05a0278a0f85879a37ae341d09
Unknown <jmverdone@gmail.com>
2017-01-14 19:12:00 -0500

    users can only comment once

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -4,17 +4,22 @@ class CommentController < ApplicationController
 
   get '/scotch/:slug/comments' do
     @scotch = Scotch.find_by_slug(params[:slug])
-    binding.pry
+
+    redirect to  "/scotch/#{@scotch.slug}" unless !@scotch.comments.find_by(user_id: current_user.id)
+
+
+    # binding.pry
     erb :'/comments/new'
   end
 
   post '/scotch/:slug/comments' do
     @scotch = Scotch.find_by_slug(params[:slug])
-
-
     Comment.create(content: params[:comment], scotch_id: @scotch.id, user_id: current_user.id)
 
     redirect to  "/scotch/#{@scotch.slug}"
   end
 
 end
+
+
+# if find_by(user_id: current_user.id, scotch_id: @scotch.id).nil?  redirect_to new_comment_page`
