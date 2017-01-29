fe3256b24dff317d3a0a94165e4585ef3e5690d7
Unknown <jmverdone@gmail.com>
2017-01-29 18:32:06 -0500

    validates through active record

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,13 +22,13 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    if params[:username] == "" || params[:password] == "" || params[:email] == ""
-      redirect to "/signup"
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if @user.save
+        session[:user_id] = @user.id
+        redirect to "/user/#{current_user.slug}"
     else
-      User.create(username: params[:username], email: params[:email], password: params[:password])
-      @user = User.find_by(username: params[:username])
-      session[:user_id] = @user.id
-      redirect to "/user/#{current_user.slug}"
+      flash[:message] = @user.errors.full_messages
+      redirect to "/user/signup"
     end
   end
 
